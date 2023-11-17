4952b1e4e8b4f47067278096dd3ac960a21d7fbc
JayM-Oh <277ipjk53@gmail.com>
2023-11-18 00:12:29 +0900

    feat(controller.Luancher): Add new class 'Luancher'
    * it roles managing game flows and logic of the game

---
+++ controller/GameManager.py
@@ -0,0 +1,4 @@
+import pygame
+from pygame.rect import *
+import random
+
---
+++ controller/Luancher.py
@@ -0,0 +1,65 @@
+import pygame
+from pygame.rect import *
+import random
+
+class Launcher:
+    def __init__(self):
+        # 초기화 관련 코드
+        self.setup_game()
+        self.setup_score()
+        self.setup_obstacles()
+        self.setup_benefits()
+
+    def setup_game(self):
+        # 게임 설정 초기화
+        pass
+
+    def setup_score(self):
+        # 점수 시스템 초기화
+        pass
+
+    def setup_obstacles(self):
+        # 장애물 관련 설정 초기화
+        pass
+
+    def setup_benefits(self):
+        # 베네핏 관련 설정 초기화
+        pass
+
+    def start_game(self):
+        # 게임 시작 로직
+        pass
+
+    def update_game_state(self):
+        # 게임 상태 (타이머, 국면 등) 업데이트
+        pass
+
+    def handle_player_input(self):
+        # 플레이어 입력 처리
+        pass
+
+    def update_obstacles(self):
+        # 장애물 업데이트
+        pass
+
+    def update_benefits(self):
+        # 베네핏 업데이트
+        pass
+
+    def check_collisions(self):
+        # 충돌 감지 및 처리
+        pass
+
+    def update_score(self):
+        # 점수 업데이트
+        pass
+
+    def render_game(self):
+        # 게임 렌더링 (그리기)
+        pass
+
+    def end_game(self):
+        # 게임 종료 로직
+        pass
+
+
